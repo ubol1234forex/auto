b20d4935f456a1452dd5533543475e42cdc54057
ubol1234forex <ubol1234forex@gmail.com>
2024-08-25 21:32:47 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+yes | pkg update && pkg upgrade
+yes | pkg install libjansson build-essential clang binutils git dialog
+yes | pkg install python3
+yes | pkg install libjansson wget nano
+
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
+
+# Clone repository 
+
+git clone https://github.com/ubol1234forex/jk8180.git
+cd jk8180
+mkdir set-miner-off
+chmod +x start.sh move.sh
+
+
+#ccminer
+mkdir ccminer && cd ccminer
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
+chmod +x ccminer 
